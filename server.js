38a0b263fcf070578461ec5dc6f3e0ed5f4890ce
DC Lechok <dclechok@gmail.com>
2026-02-22 15:01:46 -0500

    updating cors address for frontend

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ const httpServer = createServer(app);
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "sos-client-ce1q76c6o-dclechoks-projects.vercel.app"
+  "sos-client-ce1q76c6o-dclechoks-projects.vercel.app",
+  "https://lornonline.com",
+  "https://www.lornonline.com",
   // Add your production client origin(s) here:
   // "https://your-vercel-app.vercel.app",
 ];
